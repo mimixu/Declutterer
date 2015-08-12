b683e0573c0817ad3fd7b5a3f6f36d182b4c3b04
Sebastian Pierce-Durance <spiercedurance@gmail.com>
2015-08-11 20:37:58 -0700

    Added ability to toggle extension on/off

--- declutter.js
+++ declutter.js
@@ -1,9 +1,15 @@
+window.useDeclutter = false;
+
 function removeElement($el) {
     $el.hide();
 }
 
-$('body').click(function(e) {
-    e.preventDefault();
-    var $target = $(e.target);
-    removeElement($target);
-});
+var declutter = function(event) {
+    if (window.useDeclutter) {
+        event.preventDefault();
+        var $target = $(event.target);
+        removeElement($target);
+    }
+};
+
+$('body').click(declutter);
